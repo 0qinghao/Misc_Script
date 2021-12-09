fc73840206022938ecb7e430cf4d574e949a62d0
qhlin <qhlin96@foxmail.com>
2021-12-09 17:08:53 +0800

    按块分类 open/med 效果差，回到按像素点测试

--- pre_filter/filter_with_texture_map.m
+++ pre_filter/filter_with_texture_map.m
@@ -6,15 +6,19 @@ function filtered = filter_with_texture_map(in, map)
 
     for i = 1:4:size(in, 1) - 4
         for j = 1:4:size(in, 2) - 4
-            if (map(i, j) == 0)
-                if (i == size(in, 1) - 4 || j == size(in, 2) - 4)
-                    src_sub = in(i:i + 3, j:j + 3);
-                else
-                    src_sub = in(i:i + 4, j:j + 4);
-                end
 
+            if (i == 1 || j == 1)
+                src_sub = in(i:i + 3, j:j + 3);
+            else
+                src_sub = in(i - 1:i + 3, j - 1:j + 3);
+            end
+
+            if (map(i, j) == 0)
                 filtered_sub = imopen(src_sub, se);
-                filtered(i:i + 3, j:j + 3) = filtered_sub(1:4, 1:4);
+                filtered(i:i + 3, j:j + 3) = filtered_sub(end - 3:end, end - 3:end);
+            else
+                filtered_sub = med_smooth(src_sub);
+                filtered(i:i + 3, j:j + 3) = filtered_sub(end - 3:end, end - 3:end);
             end
         end
     end
