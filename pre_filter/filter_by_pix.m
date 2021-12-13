d169cebeb3b69f3f4f2f0741b9497dc950af4204
qhlin <qhlin96@foxmail.com>
2021-12-13 19:47:58 +0800

    小测试

--- pre_filter/filter_by_pix.m
+++ pre_filter/filter_by_pix.m
@@ -3,19 +3,30 @@ function out = filter_by_pix(in, TH)
     out = in;
     [h, w] = size(in);
 
-    Lap_mat = [
-            0, 1, 0;
-            1, -4, 1;
-            0, 1, 0;
-            ];
+    G_mat_1 = [0, 0, 0; 1, -2, 1; 0, 0, 0];
+    G_mat_2 = [0, 1, 0; 0, -2, 0; 0, 1, 0];
+    G_mat_3 = [1, 0, 0; 0, -2, 0; 0, 0, 1];
+    G_mat_4 = [0, 0, 1; 0, -2, 0; 1, 0, 0];
+
+    % Lap_mat = [
+    %         0, 1, 0;
+    %         1, -4, 1;
+    %         0, 1, 0;
+    %         ];
 
     for j = 2:h - 1
         for k = 2:w - 1
             src_sub = in(j - 1:j + 1, k - 1:k + 1);
-            tex_level = sum(sum(src_sub .* Lap_mat));
 
-            if (abs(tex_level) > TH)
-                out(j, k) = in(j, k) - tex_level;
+            G1 = abs(sum(src_sub .* G_mat_1, [1, 2]));
+            G2 = abs(sum(src_sub .* G_mat_2, [1, 2]));
+            G3 = abs(sum(src_sub .* G_mat_3, [1, 2]));
+            G4 = abs(sum(src_sub .* G_mat_4, [1, 2]));
+
+            isNoise = min([G1, G2, G3, G4]) > TH;
+
+            if (isNoise)
+                out(j, k) = round((sum(src_sub, [1, 2]) - in(j, k))/8);
             else
                 ;
             end
--- pre_filter/pre_filter.m
+++ pre_filter/pre_filter.m
@@ -1,15 +1,15 @@
 vid = 'D:\FH420_300f\hk_floor_12M_1920x1088.yuv';
-Tex_th = 28;
+TH = 10;
 
 [y, u, v] = yuvRead(vid, 1920, 1088, 1);
 
-y_f = filter_by_pix(y, 999);
-u_f = filter_by_pix(u, 6);
-v_f = filter_by_pix(v, 6);
+y_f = filter_by_pix(y, TH);
+u_f = filter_by_pix(u, TH/2);
+v_f = filter_by_pix(v, TH/2);
 
-% y_texture_map = get_texture_map(y, Tex_th);
-% u_texture_map = get_texture_map(u, Tex_th);
-% v_texture_map = get_texture_map(v, Tex_th);
+% y_texture_map = get_texture_map(y, TH);
+% u_texture_map = get_texture_map(u, TH);
+% v_texture_map = get_texture_map(v, TH);
 % y_f = filter_with_texture_map(y, y_texture_map);
 % u_f = filter_with_texture_map(u, u_texture_map);
 % v_f = filter_with_texture_map(v, v_texture_map);
